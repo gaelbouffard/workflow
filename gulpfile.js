06aa4c2786a0597399d55b1cc9aad3ea5ce94f94
Gael Bouffard <gaelbouffard@MacBook-Pro-de-Gael-2.local>
2018-11-04 09:05:37 -0500

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var path = {
+  root: 'app/',
+  styles: {
+    src: 'src/scss/*.scss',
+    dest: 'app/css'
+  },
+  js: 'app/js'
+}
+
+
 
 
 
 
 function styles() {
-  return gulp.src('src/scss/app.scss')
+  return gulp
+    .src(path.styles.src)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest(path.styles.dest));
 };
-
 exports.styles = styles;
 
-var build = gulp.series(gulp.parallel(styles));
+function serve(){
+  browserSync.init([path.styles.dest + '/*.css', path.root + '/*.html', path.js + '/*.js'], {
+    server: {
+      baseDir : path.root
+    }
+  })
+}
+
+function watch(){
+  styles();
+  gulp.watch(path.styles.src, styles);
+}
+
+// exports.styles = styles;
+// exports.serve = serve;
+
+var build = gulp.series(gulp.parallel(serve, watch));
+
+
 
 gulp.task('default', build);
